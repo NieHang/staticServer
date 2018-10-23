4d235da96720fd5b21f428065ba5018fa93a414b
NieHang <1076883711@qq.com>
2018-10-23 16:02:57 +0800

    ready

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parser": 'babel-eslint',
+  "parserOptions": {
+    "ecmaVersion": 2015,
+    "sourceType": 'script'
+  },
+  "rules": {
+    "no-console": [
+      "error",
+      {
+        "allow": ['warn', 'error', 'info']
+      }
+    ],
+    "indent": [
+      "error",
+      "tab"
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
